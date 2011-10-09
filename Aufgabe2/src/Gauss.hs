da82076b6ec4b93515e9a241127ede75f9c3028b
Robert Clausecker <fuzxxl@gmail.com>
2011-10-10 00:56:57 +0200

    Task 2: Adopt first echelon form to fit the code's output

--- Aufgabe2/src/Gauss.hs
+++ Aufgabe2/src/Gauss.hs
@@ -40,19 +40,19 @@ gaussianElemination (Matrix m) = Matrix $ go 0 m where
 
 -- TEST TEST TEST
 
-m7,m9 :: Matrix ()
-
+m7  :: Matrix Bool
 -- m7: Erste Beispielmatrix
 m7 = read
-  "1 1 0 0 0 0 1\n\
-  \1 1 1 0 0 0 0\n\
-  \0 1 1 1 0 0 0\n\
-  \0 0 1 1 1 0 0\n\
-  \0 0 0 1 1 1 0\n\
-  \0 0 0 0 1 1 1\n\
-  \1 0 0 0 0 1 1"
+  "1 1 0 0 0 0 1 | 0\n\
+  \1 1 1 0 0 0 0 | 0\n\
+  \0 1 1 1 0 0 0 | 1\n\
+  \0 0 1 1 1 0 0 | 0\n\
+  \0 0 0 1 1 1 0 | 1\n\
+  \0 0 0 0 1 1 1 | 0\n\
+  \1 0 0 0 0 1 1 | 1"
 
 -- m9: Zweite Beispielmatrix
+m9 :: Matrix ()
 m9 = read
   "1 1 0 1 0 0 0 0 0\n\
   \1 1 1 0 1 0 0 0 0\n\
